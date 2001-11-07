5943595a76dc527e7ae50e0f3399149d0cd6d6bf
Tyler Akins <fidian@rumkin.com>
2001-11-07 17:48:52 +0000

    Added mailing list information Added section about testing the install of PHP-PDB

--- doc/extra.php
+++ doc/extra.php
@@ -6,10 +6,22 @@ StandardHeader('Extra Information');
 
 ?>
 
+<h3>Making sure PHP-PDB works</h3>
+
+<p>After you installed PHP-PDB somewhere, just use your web browser and view
+<tt>pdb-test.php</tt>.  It will perform a few tests and tell you the results
+of the tests.  They should all say <font color=green>pass</font>.  If they
+don't, contact me for help.</p>
+
 <h3>Contact information</h3>
 
-<p>Well, I didn't get a mailing list set up yet, but I will soon.  Until
-then, just email me directly (fidian at tiny dot net).</p>
+<p>I finally have mailing lists set up.  If you are asking for help, please
+subscribe to the mailing list and ask your question.  Your answer should
+come to you pretty fast.  If you abhore mailing lists and would rather have
+something painful happen to you instead of subscribing to a mailing list,
+just email me directly.  I'll simply forward your request to the mailing
+list and then ask that the list members reply to you and to the list.  My
+email address is (fidian at tiny dot net).</p>
 
 <h3>Legalese</h3>
 
