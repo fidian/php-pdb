dd1b77f0d64dc4e2bd6287018f3f06a81956b059
Tyler Akins <fidian@rumkin.com>
2001-11-10 14:38:24 +0000

    Added more documentation

--- doc/extra.php
+++ doc/extra.php
@@ -29,7 +29,21 @@ email address is (fidian at tiny dot net).</p>
 href="http://www.gnu.org/copyleft/lesser.html">GNU LGPL</a> and is basically
 free to use for everyone.</p>
 
+<h3>Still to do</h3>
+
+<ul>
+<li>Add <a
+href="http://www.handmark.com/products/mobiledb/dbstructure.htm">MobileDB</a>
+support.</li>
+<li>Maybe look closer at <a href="http://mmmm.free.fr/palm/">palmlib</a></li>
+<li>Wouldn't image support from <a
+href="http://palmimage.sourceforge.net/">PalmImage</a> be grand?</li>
+</ul>
+
 <h3>Credits and thanks</h3>
+
+<p>I did actually write all code from scratch, but credit should be given to
+people who wrote other open-source programs that I learned from.</p>
 
 <ul>
 <li>Arensburger, Andrew -- Thanks for making <a
@@ -37,8 +51,13 @@ free to use for everyone.</p>
    several ideas when creating PHP-PDB, and a few comments really helped me
    out.</li>
 <li>Dittgen, Pierre -- His <a 
-   href="http://mmmm.free.fr/palm/download/">palmlib</a> (aka ToPTIP) was
-   handy to have around when initially adding DOC support.</li>
+   href="http://mmmm.free.fr/palm/download/">palmlib</a> (aka <a 
+   href="http://depot.free-system.com/index.php3?projectid=2&action=show"
+   >ToPTIP</a>) was handy to have around when initially adding DOC support
+   to compare output and make sure mine worked.</li>
+<li><a href="http://www.palm.com">Palm Computing, Inc.</a> -- Without them,
+   there would not be a PalmOS.  Also, they provided documentation that made
+   my life much easier.</li>
 </ul>
 
 <?PHP
