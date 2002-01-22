58616230fc4cef94cb635299ef72f90caad055d0
Tyler Akins <fidian@rumkin.com>
2002-01-22 14:28:24 +0000

    Moved // START HERE up a few lines to where it should be Rewrapped the text so that it should look better when viewing the source of this file on the web page.

--- doc/samples/bookmark_test.php
+++ doc/samples/bookmark_test.php
@@ -15,6 +15,8 @@ if (file_exists('../../php-pdb.inc')) {
    include('./modules/doc.inc');
 }
 
+// START HERE
+
 if (! isset($Stored)) {
 ?>
 <html><head><title>Bookmark Test Generator</title>
@@ -27,8 +29,6 @@ if (! isset($Stored)) {
 <?PHP
    exit();
 }
-
-// START HERE
 
 if ($Stored)
    $pdb = new PalmDoc("Stored Bookmark Test");
@@ -45,17 +45,18 @@ if ($Stored) {
    $UcFirst = "Embedded";
 }
 
-$pdb->AddDocText("This is a test file to see if $ThisTest bookmarks " .
-"are recognized by a document reader.  To determine if $OtherTest " .
-"bookmarks are supported, you'll need to use the other bookmark test that " .
-"you got from $PHP_SELF\n\n" .
-"To figure out if it worked, look for the bookmark list in this document " .
-"reader.  If it has a bookmark named \"$UcFirst Bookmark\", then it " .
-"worked.  If not, then this document reader doesn't support $ThisTest " .
-"style bookmarks.\n\n" .
-"This document was automatically generated using the PHP-PDB library -- a " .
-"free, GPL'd PHP library that manipulates PDB files.  Feel free to give " .
-"it a look at\nhttp://php-pdb.sourceforge.net\n\n");
+$pdb->AddDocText("This is a test file to see if $ThisTest " .
+"bookmarks are recognized by a document reader.  To determine if " .
+"$OtherTest bookmarks are supported, you'll need to use the " .
+"other bookmark test that you got from $PHP_SELF\n\n" .
+"To figure out if it worked, look for the bookmark list in this " .
+"document reader.  If it has a bookmark named \"$UcFirst " .
+"Bookmark\", then it worked.  If not, then this document reader " .
+"doesn't support $ThisTest style bookmarks.\n\n" .
+"This document was automatically generated using the PHP-PDB " .
+"library -- a free, GPL'd PHP library that manipulates PDB " .
+"files.  Feel free to give it a look at\n" .
+"http://php-pdb.sourceforge.net\n\n");
 
 // Add the bookmark
 if ($Stored)
@@ -63,35 +64,38 @@ if ($Stored)
 else
    $pdb->AddDocText("*Embedded Bookmark\n");
    
-$pdb->AddDocText("This is where \"$UcFirst Bookmark\" should take you, if " .
-"this document reader properly supports $ThisTest bookmarks.\n\n" .
-"Don't forget to see if your document reader supports $OtherTest style " .
-"bookmarks!\n\n");
+$pdb->AddDocText("This is where \"$UcFirst Bookmark\" should " .
+"take you, if this document reader properly supports $ThisTest " .
+"bookmarks.\n\n" .
+"Don't forget to see if your document reader supports $OtherTest " .
+"style bookmarks!\n\n");
 
 $pdb->AddDocText("Have a GREAT day!\n\n");
 
 if (! $Stored)
-   $pdb->AddDocText("This next part is needed by the document reader " .
-   "to detect where embedded bookmarks are located.  If you plan on " .
-   "generating documents with embedded bookmarks, you just need to add " .
-   "the special character (I'm using an asterisk -- the star thing) at " .
-   "the beginning of the line and the rest of the line becomes the " .
-   "bookmark.  Then, you just include \"<X>\" at the end of your " .
-   "document (replacing the X with the character you picked) to tell " .
-   "the doc reader which character you used to define bookmarks, like " .
-   "what I have at the end of this one.\n\n" .
-   "Please note:  Aportis will use the special character if it appears " .
-   "anywhere inside the file, so try to pick a character that is not " .
-   "in the text you are converting into a doc file:\n\n" .
+   $pdb->AddDocText("This next part is needed by the document " .
+   "reader to detect where embedded bookmarks are located.  If " .
+   "you plan on generating documents with embedded bookmarks, " .
+   "you just need to add the special character (I'm using an " .
+   "asterisk -- the star thing) at the beginning of the line and " .
+   "the rest of the line becomes the bookmark.  Then, you just " .
+   "include \"<X>\" at the end of your document (replacing the X " .
+   "with the character you picked) to tell the doc reader which " .
+   "character you used to define bookmarks, like what I have at " .
+   "the end of this one.\n\n" .
+   "Please note:  Aportis will use the special character if it " .
+   "appears anywhere inside the file, so try to pick a character " .
+   "that is not in the text you are converting into a doc " .
+   "file:\n\n" .
    "<*>");
 else
-   $pdb->AddDocText("This is just some garbage that should fill the " .
-   "screen so that when you jump to the stored bookmark, the message " .
-   "saying that you are in the right spot should be immediately at the " .
-   "top.  If this text is not there, and if you use a small font, you " .
-   "may see it in the middle or closer to the bottom.  This is " .
-   "annoying, and to keep things simple, I just add this filler material " .
-   "at the bottom.");
+   $pdb->AddDocText("This is just some garbage that should fill " .
+   "the screen so that when you jump to the stored bookmark, the " .
+   "message saying that you are in the right spot should be " .
+   "immediately at the top.  If this text is not there, and if " .
+   "you use a small font, you may see it in the middle or closer " .
+   "to the bottom.  This is annoying, and to keep things simple, " .
+   "I just add this filler material at the bottom.");
 
 $pdb->DownloadPDB("bookmark_" . $ThisTest . ".pdb");
 
