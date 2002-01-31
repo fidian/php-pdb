5e43f3d74bca0a25da28be01b5ab04e8986b94cb
Tyler Akins <fidian@rumkin.com>
2002-01-31 16:13:37 +0000

    Added the spiffy Download page, with all of its coded goodnes.

---
+++ doc/download.php
@@ -0,0 +1,60 @@
+<?PHP
+/* Documentation for PHP-PDB library
+ *
+ * Copyright (C) 2001 - PHP-PDB development team
+ * Licensed under the GNU LGPL software license.
+ * See the doc/LEGAL file for more information
+ * See http://php-pdb.sourceforge.net/ for more information about the library
+ */
+
+include("./functions.inc");
+
+StandardHeader('Download PHP-PDB');
+
+?>
+
+<h3>Standard download</h3>
+
+<p>Whenever the code feels mature enough for release, a new package is
+created.  Feel free to download the latest version (in .zip, .tar.bz2, or
+.tar.gz formats) from PHP-PDB's
+<a href="http://sourceforge.net/project/showfiles.php?group_id=29740">
+download page</a> on SourceForge.</p>
+
+<h3>CVS version</h3>
+
+<p>A true geek desires the latest additions, bugfixes, features, and
+everything else before it is officially bundled up and released to the
+public.  The CVS version is the 'live' copy of PHP-PDB.  If something minor
+was changed or alpha support for a new format is added, it will show up
+first in CVS.  After it hangs around a bit, the CVS version will be bundled
+as a standard release.</p>
+
+<p>If you are like me and you can't wait for all of the latest and greatest
+coded goodness, you can fire up your CVS program with the
+<a href="http://sourceforge.net/cvs/?group_id=29740">instructions</a> that
+SourceForge provides.  (The module name is <tt>php-pdb</tt>.)</p>
+
+<h3>Nightly tarballs</h3>
+
+<p>Some systems don't use CVS.  Some don't like CVS.  If you are a user and
+not a sysadmin, you might not have any access to CVS.  Don't worry one bit --
+SourceForge has you covered!  For the purposes of archival, SourceForge
+makes nightly tarballs of the CVS tree.  This means you too can have the
+latest and greatest coded goodness in your machine!  Just download
+<a href="http://cvs.sourceforge.net/cvstarballs/php-pdb-cvsroot.tar.gz">
+php-pdb-cvsroot.tar.gz</a> and you'll be on your way to PHP-PDB bliss.
+
+<h3>Old versions</h3>
+
+<p>If you have CVS, you can get old versions.  Just read the CVS man page or
+documentation.  If you don't, I have a few of the older versions available
+for download from SourceForge.  Just head on over to the 
+<a href="http://sourceforge.net/project/showfiles.php?group_id=29740">
+download page</a> and pick whatever ancient version you want.</p>
+
+<?PHP
+
+StandardFooter();
+
+?>
